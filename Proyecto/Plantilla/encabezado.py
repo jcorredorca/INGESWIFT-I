041a4fdceb653c94c7e617fd60039c089999d343
n-bolanos <nbolanosf@unal.edu.co>
2025-07-01 15:58:38 -0500

    resize encabezado

--- Proyecto/Plantilla/encabezado.py
+++ Proyecto/Plantilla/encabezado.py
@@ -8,34 +8,50 @@ class Encabezado(CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.configure(fg_color="white", corner_radius=1, )
+        self.configure(fg_color="white", corner_radius=1 )
         self.repartir_espacio()
 
-        logo_un,logo_atun = self.crear_imagenes_ctk()
-        self.logo_atun = CTkLabel(self, image=logo_atun, text='')
-        self.logo_atun.grid(row=0, column=0, sticky="nw", pady=10, padx=10)
+        self.abrir_imagenes()
+        
+        self.logo_atun = CTkLabel(self, text='')
+        self.logo_atun.grid(row=0, column=0, sticky="nw")
 
+        self.logo_un = CTkLabel(self, text='')
+        self.logo_un.grid(row=0, column=2, sticky="ne")
+
+        self.actualizar_dimensiones_imagen()
         self.links = None
-
-        self.logo_un = CTkLabel(self, image=logo_un, text='')
-        self.logo_un.grid(row=0, column=2, sticky="ne",  padx=10)
+ 
 
     def repartir_espacio(self):
         '''Reparte el espacio '''
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=2)
+        self.grid_columnconfigure(1, weight=3)
         self.grid_columnconfigure(2, weight=1)
 
-    def crear_imagenes_ctk(self):
-        '''Este metodo crea los objetos imagen para mostrarlo en un label'''
-
+    def abrir_imagenes(self):
+        '''Este método abre las imágenes necesarias para el encabezado'''
         base_dir = path.dirname(__file__)
         # Rutas a imagenes
         ruta_logo_un = path.join(base_dir,"..","Imagenes", "LogoUNAL.png")
         ruta_logo_atun = path.join(base_dir,"..","Imagenes", "LogoATUN.png")
 
-        logo_un = CTkImage(light_image=Image.open(ruta_logo_un),
-        size=(370//1.5, 200//1.5))
-        logo_atun = CTkImage(light_image=Image.open(ruta_logo_atun),
-        size=(631//2, 215//2))
-        return logo_un,logo_atun
+        self.imagen_un =Image.open(ruta_logo_un)
+        self.imagen_atun =Image.open(ruta_logo_atun)
+    
+    def actualizar_dimensiones_imagen(self):
+        '''Ajusta automáticamente las dimensiones de la imagen al frame'''
+
+        frame_heigth = self.master.winfo_height()/2
+        new_width_un = frame_heigth * 370 / 200
+        new_width_atun = frame_heigth * 631 / 215
+
+        logo_un = CTkImage(light_image=self.imagen_un, size=(new_width_un, frame_heigth))
+        logo_atun = CTkImage(light_image=self.imagen_atun, size=(new_width_atun, frame_heigth))
+
+        self.logo_atun.configure(image=logo_atun)
+
+        self.logo_un.configure(image=logo_un)
+
+        self.master.update()
--- Proyecto/Plantilla/ventana_principal.py
+++ Proyecto/Plantilla/ventana_principal.py
@@ -12,38 +12,38 @@ class App(customtkinter.CTk):
 
     def __init__(self):
         super().__init__()
-     
+
         self.title("ATUN")
         self.agregar_fuente()
-        self.configure(fg_color = '#2e1045')
+        self.configure(fg_color = "#09FF00")
         self.configurar_dimensiones()
         self.repartir_espacio()
 
         #Colocamos los 3 elementos principales
         self.encabezado = encabezado.Encabezado(self)
-        self.encabezado.grid(row=0, column=0, sticky="ew")
+        self.encabezado.grid(row=0, column=0, sticky="nsew")
 
         self.contenido = inicio.Inicio(self)
         self.contenido.grid(row=1, column=0, sticky="nsew")
 
         self.pie = pie_de_pagina.PiePagina(self)
-        self.pie.grid(row=2, column=0, sticky="ew")
+        self.pie.grid(row=2, column=0, sticky="nsew")
 
     def configurar_dimensiones(self):
         '''Hace las configuraciones iniciales en cuanto a tamaños de pantalla'''
+        self.activar_fullscreen()
         ancho,alto = self.get_size()
         self.geometry(f"{ancho}x{alto}+0+0")
-        self.minsize(width=1500, height=1100)
-        self.activar_fullscreen()
+        self.minsize(width=800, height=600)
         self.bind("<F11>", lambda e: self.activar_fullscreen())   # F11 para activar
         self.bind("<Escape>", lambda e: self.desactivar_fullscreen())  # ESC para salir
 
     def repartir_espacio(self):
         '''Reparte el espacio entre el encabezado contenido y pie de pagina'''
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=0)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=0)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=4)
+        self.grid_rowconfigure(2, weight=1)
 
     def get_size(self):
         '''Trae las dimensiones de la pantalla actual'''
