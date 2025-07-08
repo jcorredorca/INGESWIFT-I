5ecbc74ba3e59fdbf5d0467bb69fec9f08dc4b05
Santi798 <santiagoandresfetecuapulgarin@gmail.com>
2025-07-07 20:37:50 -0500

    mejoras ventana miembros

--- Proyecto/views/miembros/miembros.py
+++ Proyecto/views/miembros/miembros.py
@@ -1,5 +1,8 @@
 ''' Vista del panel de horarios para miembros'''
-from customtkinter import CTkFrame,CTkOptionMenu
+from os import path
+from config import IMG_PATH
+from PIL import Image
+from customtkinter import CTkFrame, CTkOptionMenu, CTkImage, CTkLabel
 from ..components.horario_semanal import HorarioSemanal
 from ..components.boton_adicional import BotonAdicional
 from core import utils
@@ -9,13 +12,21 @@ class Miembros(CTkFrame):
     ''' Esta clase representa el panel de horarios para miembros '''
     def __init__(self, master):
         super().__init__(master)
+
         self.configure(fg_color= "#2e1045", corner_radius=1)
         self.repartir_espacio()
 
         self.crear_menu_opciones()
 
+        self.abrir_imagen()
+
+        self.imagen_convencion_label = CTkLabel(self, text='')
+        self.imagen_convencion_label.grid(row=2, column=1, sticky="ns")
+
+        self.actualizar_dimensiones_imagen()
+
         self.horario = HorarioSemanal(self)
-        self.horario.grid(row=1, column=3, sticky='e')
+        self.horario.grid(row=1, column=3, sticky='e', rowspan=2)
 
         self.logout = BotonAdicional(master.encabezado.logout,
                                     texto='LogOut', comando= self.llamar_a_logout)
@@ -27,7 +38,8 @@ class Miembros(CTkFrame):
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=0)
-        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(2, weight=0)
+        self.grid_rowconfigure(3, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=0)
         self.grid_columnconfigure(2, weight=1)
@@ -49,6 +61,30 @@ class Miembros(CTkFrame):
         values= ['Escoge tu Plan'] + miembros.recuperar_actividades()
         )
         self.opciones_busqueda.grid(row=1, column=1, sticky = 'n', padx=(5,0) )
+
+    def abrir_imagen(self):
+        '''Este metodo crea los objetos imagen para mostrarlo en un label'''
+
+        # Rutas a imagen
+        ruta_convencion_img = path.join(IMG_PATH, "Convencion.png")
+
+        self.imagen_convencion = Image.open(ruta_convencion_img)
+
+
+    def actualizar_dimensiones_imagen(self):
+        '''Ajusta automáticamente las dimensiones de la imagen al frame'''
+
+        factor = 1/6
+        frame_height = self.master.winfo_screenheight()*factor
+
+        new_width = frame_height* 407/277
+
+        imagen_inicio_tk = CTkImage(light_image=self.imagen_convencion,
+                                    size=(new_width,frame_height))
+
+        self.imagen_convencion_label.configure(image= imagen_inicio_tk)
+
+        self.master.update()
 
     def llamar_a_logout(self):
         '''Este metodo llama a la funcion logout'''
